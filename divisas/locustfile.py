92cfb1f37b72c4b765e3329517f3335a090d6757
Juan David Fajardo Betancourt <jfajardob@unal.edu.co>
2025-09-10 19:51:59 -0500

    se añaden comentarios del locusfile sobre pruebas de rendimiento

--- divisas/locustfile.py
+++ divisas/locustfile.py
@@ -4,11 +4,17 @@
 #Import 
 from locust import HttpUser, task
 
+# clase usuario que usa un ejemplo de HttpUser como instancia 
 class SimpleUser(HttpUser):
+    """
+    Testeamos los metodos de CRUD con una api super simple 
+    """
+    #hacemos test del metodo Get
     @task
     def get_product(self):
         self.client.get("/")
     
+    #hacemos test del metodo post
     @task
     def create_product(self):
         data = {
@@ -20,6 +26,7 @@ class SimpleUser(HttpUser):
         }
         self.client.post("/", json=data)
 
+    #hacemos test del metodo put
     @task
     def update_product(self):
         data = {
@@ -32,6 +39,7 @@ class SimpleUser(HttpUser):
         }
         self.client.put("/1", json=data)
 
+    #hacemos test del metodo Delete
     @task
     def delete_product(self):
         self.client.delete("/1")
