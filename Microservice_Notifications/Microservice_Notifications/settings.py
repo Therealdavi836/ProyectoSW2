e72680bdf171f87b4bee992fd8d4b5d1b95f0108
Juan David Fajardo Betancourt <jfajardob@unal.edu.co>
2025-10-15 21:38:27 -0500

    se descomentan lineas para prueba real

--- Microservice_Notifications/Microservice_Notifications/settings.py
+++ Microservice_Notifications/Microservice_Notifications/settings.py
@@ -149,9 +149,9 @@ CORS_ALLOW_HEADERS = ["*"]
 CORS_ALLOW_METHODS = ["*"]
 
 # Configuración de correo
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # Para pruebas en desarrollo
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
--- Microservice_Notifications/notifications/views.py
+++ Microservice_Notifications/notifications/views.py
@@ -60,7 +60,7 @@ class NotificationViewSet(viewsets.ModelViewSet):
             print(f"Error al enviar correo: {e}")
 
         # --- Envío de SMS ---
-        """ try:
+        try:
             sid = getattr(settings, "TWILIO_ACCOUNT_SID", None)
             token = getattr(settings, "TWILIO_AUTH_TOKEN", None)
             from_number = getattr(settings, "TWILIO_PHONE_NUMBER", None)
@@ -77,6 +77,6 @@ class NotificationViewSet(viewsets.ModelViewSet):
             else:
                 print("Configuración de Twilio incompleta en settings.py")
         except Exception as e:
-            print(f"Error al enviar SMS: {e}") """
+            print(f"Error al enviar SMS: {e}")
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
